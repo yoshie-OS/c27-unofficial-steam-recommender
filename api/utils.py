0f1f8bc524c25166b9569e4ba0d52f80f3075b9d
yoshie <pranedyaadhi@proton.me>
2025-05-15 11:25:21 +0700

    Debugging

--- api/utils.py
+++ api/utils.py
@@ -1,17 +1,22 @@
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
+import streamlit as st
 
 def decrypt_api_key():
     """Decrypt the API key using the encryption key"""
-    # Load the encryption key
-    with open('./.streamlit/encryption_key.key', 'rb') as key_file:
-        encryption_key = key_file.read()
+    # # Load the encryption key
+    # with open('./.streamlit/encryption_key.key', 'rb') as key_file:
+    #     encryption_key = key_file.read()
 
-    # Load the encrypted API key
-    with open('./.streamlit/encrypted_api_key.bin', 'rb') as file:
-        encrypted_api_key = file.read()
+    # # Load the encrypted API key
+    # with open('./.streamlit/encrypted_api_key.bin', 'rb') as file:
+    #     encrypted_api_key = file.read()
 
-    # Decrypt the API key
-    fernet = Fernet(encryption_key)
-    api_key = fernet.decrypt(encrypted_api_key).decode()
+    # # Decrypt the API key
+    # fernet = Fernet(encryption_key)
+    # api_key = fernet.decrypt(encrypted_api_key).decode()
 
-    return api_key
+    try:
+        return st.secrets["api_keys"]["steam"]
+    except Exception as e:
+        st.error(f"Error accessing Steam API key: {e}")
+        return None
